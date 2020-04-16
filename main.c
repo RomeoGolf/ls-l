b564a3cceae0b0ced7f3ab50057ab4509a5387b9
RomeoGolf <romeogolf@local>
2020-04-16 10:15:40 +0500

    A current dir reading was added

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello!\n");
+    DIR *dir = opendir("./");
+    if (!dir) {
+        perror("Can not open dir");
+        return 1;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%d - %s [%d] %d\n",
+                entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
+    }
+    closedir(dir);
+
     return 0;
 }
